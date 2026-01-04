cfa7e0c22ae56413956d79b24bf3cb8246da9861
Noah-school <noah.verstraeten@student.ap.be>
2026-01-04 07:12:15 -0800

    Sync modules/system_enum.py

--- modules/system_enum.py
+++ modules/system_enum.py
@@ -3,6 +3,16 @@ import os
 import psutil
 import json
 
+def get_processor_name():
+    if platform.system() == "Linux":
+        try:
+            with open("/proc/cpuinfo", "r") as f:
+                for line in f:
+                    if "model name" in line:
+                        return line.split(":")[1].strip()
+        except Exception: pass
+    return platform.processor()
+
 def run(**args):
     print("[*] In system_enum module.")
     info = {
@@ -12,7 +22,7 @@ def run(**args):
         "architecture": platform.machine(),
         "hostname": platform.node(),
         "username": os.getlogin() if hasattr(os, 'getlogin') else "unknown",
-        "processor": platform.processor(),
+        "processor": get_processor_name(),
         "processes": [p.info for p in psutil.process_iter(['pid', 'name', 'username'])][:20]
     }
     return json.dumps(info)
